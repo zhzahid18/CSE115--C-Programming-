890f88ec5a152885cb7709b3969e58261f48e00a
Zahid Hasan <zahid.hasan.1911509642@gmail.com>
2020-06-19 09:09:44 +0600

    Update CricketGame.c

--- CricketGame.c
+++ CricketGame.c
@@ -3,10 +3,10 @@
 #include <time.h>
 int main()
 {
-    int m,cpuScore=0, myScore=0;
+    int m,cpuScore=0, myScore=0, over=0;
     srand(time(0));
     printf("\t\t\t\t:)_:)_30 balls cRiCkEt mAtCh_:)_:)");
-    printf("\n\t\t\t\tGame Characteristic & Rules:");
+    r:printf("\n\t\t\t\tGame Characteristic & Rules:");
     printf("\n\t\t1)Actually it is a TARGET CHASING cricket game. So, Score of CPU will be generated automatically");
     printf("\n\t\t2)If you hit 0 or greater than 6, then it will be counted as DOT BALL(No Score). And also if your hitted score matches with random generated number, ");
     printf("\n\t\tit will be counted as DOT BALL(No Score)");
@@ -37,6 +37,38 @@ int main()
             printf("\n\tHit to score: ");
             scanf("%d",&input);
             int num2= (rand() % 6)+1;
+           
+            if(input==num2 || input==0 || input>6)
+            {
+                printf("\tNo Score\n");
+                num2=0;
+                input=0;
+            }
+            else
+               myScore=myScore+input;
+        }
+        if(i==6 || i==12 || i==18 || i==24)
+            printf("\nReminder:Now Your score is: %d. You need more %d runs to win",myScore,cpuScore-myScore+1);
+        printf("\n");
+        if(myScore>cpuScore)
+        {
+            printf("\n\tYour Total Scores : %d",myScore);
+            printf("\n\tCONGRATULATION! You won the GAME!!!");
+            break;
+        }
+    }
+
+    if(cpuScore>myScore){
+            printf("\n\tYour Total Scores : %d",myScore);
+        printf("\n\tCPU wins!!! You have lost match by %d runs!! Better Luck Next Time!",cpuScore-myScore);
+    }
+    else if(cpuScore==myScore){
+            printf("\n\tYour Total Scores : %d",myScore);
+        printf("\n\tWhat a GAME!! Match Tied!");
+    }
+
+}
+
 
             if(input==num2 || input==0 || input>6)
             {
